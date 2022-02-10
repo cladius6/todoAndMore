f16411ef638a115919768f5e50fec7251b6bd3f4
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-02-10 14:10:10 +0100

    No light theme.
    Only dark version now.

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -5,8 +5,8 @@ import { extendTheme } from '@chakra-ui/react'
 
 // 2. Add your color mode config
 const config = {
-  initialColorMode: 'light',
-  useSystemColorMode: true,
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
 }
 
 // 3. extend the theme
