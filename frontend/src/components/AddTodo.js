84635efaf4deaca7e07b1ac9c56b563dba33602f
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-02-12 02:37:51 +0100

    AddTodo clean

--- frontend/src/components/AddTodo.js
+++ frontend/src/components/AddTodo.js
@@ -1,32 +1,54 @@
-import { React,useState } from 'react';
-import { Box, Button, Input, Tooltip } from '@chakra-ui/react'
-import { AddIcon } from '@chakra-ui/icons'
+import { React, useState } from 'react';
+import { Box, Button, Input, Tooltip } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
 
-const AddTodo = ( {addTodo} ) => {
-	const [name, setContent] = useState('')
-	const [description, setDescription] = useState('')
+const AddTodo = ({ addTodo }) => {
+  const [name, setContent] = useState('');
+  const [description, setDescription] = useState('');
 
-	const addTodoHandler = e => {
-		e.preventDefault()
-		addTodo({name},)
-	}
-	return (
-        <form onSubmit={addTodoHandler} id='todo_add_form'>
-		<Box p='5' size='lg' display='flex' maxW='xl' bg='gray.800' alignItems='center'>
-        <Tooltip hasArrow label='Press enter to add quickly'>
-			<Input autoFocus 
-                    fontSize='32px' 
-                    size='lg' 
-                    borderRadius='0' 
-                    variant='flushed' placeholder='Name' onChange={e => setContent(e.target.value)}/>
+  const addTodoHandler = e => {
+    e.preventDefault();
+    addTodo({ name });
+  };
+
+  return (
+    <form onSubmit={addTodoHandler} id="todo_add_form">
+      <Box
+        p="5"
+        size="lg"
+        display="flex"
+        maxW="xl"
+        bg="gray.800"
+        alignItems="center"
+      >
+        <Tooltip hasArrow label="Press enter to add quickly">
+          <Input
+            autoFocus
+            fontSize="32px"
+            size="lg"
+            borderRadius="0"
+            variant="flushed"
+            placeholder="Name"
+            onChange={e => setContent(e.target.value)}
+          />
         </Tooltip>
-        <Tooltip hasArrow label='Add with more informations.'>
-			<Button leftIcon={<AddIcon />} fontSize='32px' variant='ghost' size='lg' textTransform='uppercase' borderRadius='0' colorScheme='teal' onClick={addTodoHandler}>Add
-        </Button>
+        <Tooltip hasArrow label="Add with more informations.">
+          <Button
+            leftIcon={<AddIcon />}
+            fontSize="32px"
+            variant="ghost"
+            size="lg"
+            textTransform="uppercase"
+            borderRadius="0"
+            colorScheme="teal"
+            onClick={addTodoHandler}
+          >
+            Add
+          </Button>
         </Tooltip>
-        </Box>
-        </form>
-	);
-}
+      </Box>
+    </form>
+  );
+};
 
-export default AddTodo
+export default AddTodo;
