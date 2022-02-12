4e9dd6c79116c218119b2c93d901c6e357fda2d8
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-02-12 02:35:54 +0100

    Datepicker fixed
    good look for datepicker

--- frontend/src/components/DatePicker.js
+++ frontend/src/components/DatePicker.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker';
-import { useColorMode, Input } from '@chakra-ui/react';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import '../static/date-picker.css';
 
 function DatePicker(props: ReactDatePickerProps) {
-  const {
-  isClearable = false,
-  showPopperArrow = false,
-  ...rest
-} = props
+  const { isClearable = false, showPopperArrow = false, ...rest } = props;
 
   return (
-    <div className='dark-theme'>
+    <div className="dark-theme">
       <ReactDatePicker
         isClearable={isClearable}
+        timeFormat='HH:mm'
         showPopperArrow={showPopperArrow}
         className="react-datapicker__input-text"
         {...rest}
       />
     </div>
   );
-};
+}
 
-export default DatePicker
+export default DatePicker;
